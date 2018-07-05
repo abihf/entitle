64e9f3a44ac92cde0737ebb0a95530ed22dbafa9
Abi Hafshin Alfarouq <abi.alfarouq@traveloka.com>
2018-07-05 13:49:21 +0700

    [FIX] keep now server alive

--- main.go
+++ main.go
@@ -1,11 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
+	// stupid hack to make
+	go keepAlive()
+
 	http.HandleFunc("/webhook", handleHook)
+	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "PONG")
+	})
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func keepAlive() {
+	for {
+		time.Sleep(10 * time.Second)
+		http.Get("https://entitle.now.sh/ping")
+	}
 }
